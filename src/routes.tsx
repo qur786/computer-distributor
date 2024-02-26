665292fe3608dadd08b73e7d4a7f4bcd8a7a0891
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-26 22:59:48 +0530

    refactor: use routes dict

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,17 @@
 import { App } from "./App";
+import { HomePage } from "./pages/Home";
+import { NavLinks } from "./components/utils";
 import { createBrowserRouter } from "react-router-dom";
 
 export const routes = createBrowserRouter([
   {
-    path: "/",
+    path: NavLinks.Home,
     element: <App />,
+    children: [
+      {
+        path: NavLinks.Home,
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
