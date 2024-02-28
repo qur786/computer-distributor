c4b4f3111c74e18b4e9a9e22c0e6c26d58fffd57
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 00:34:50 +0530

    feat: added Product Card

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,7 +1,15 @@
+import { ProductCard } from "../../components/ProductCard";
+
 export function ProductsPage(): JSX.Element {
   return (
-    <div className="h-screen w-full flex flex-col items-center justify-center">
-      Coming soon...
+    <div className="grid md:grid-cols-3 grid-cols-1 gap-x-8 gap-y-8 mt-12">
+      {/* TODO: remove it and use correct data */}
+      <ProductCard
+        title="Sony Xperia Digital Camera"
+        price="37000"
+        image="https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        imageAlt="Acer computer"
+      />
     </div>
   );
 }
