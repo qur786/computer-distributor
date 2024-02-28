e0ea69bb50f3ac5aedc0c44d87af2b12064e6dcf
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 00:47:37 +0530

    docs: added todo comment to add search bar logic

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -8,6 +8,7 @@ export function ProductsPage(): JSX.Element {
         <span className="text-red-600">Flash Deals,</span> Enjoy Upto 70%
         discounts
       </h3>
+      {/* TODO: add search product logic */}
       <div className="flex flex-row border rounded-xl px-4 py-2 gap-2">
         <MagnifyingGlassIcon className="h-6" />
         <input
