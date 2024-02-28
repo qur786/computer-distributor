df43ffcb66b8aa75e733c7d6f86880c3a4031457
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-28 16:45:10 +0530

    refactor: put search bar and menu icon together

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -18,29 +18,33 @@ export function Header(): JSX.Element {
   };
 
   return (
-    <header className="fixed w-full p-4 dark:bg-gray-800 dark:text-white backdrop-blur-md shadow-lg flex items-center justify-between">
-      <img
-        src={PlaceholderLogo}
-        alt="Logo"
-        className="h-12 w-28"
-        title="Logo"
-      />
-      <nav className="hidden gap-8 md:flex">
-        {Object.entries(NavLinks).map(([title, link]) => (
-          <StyledNavLink to={link} key={title}>
-            {title}
-          </StyledNavLink>
-        ))}
-      </nav>
+    <>
+      <header className="fixed w-full p-4 dark:bg-gray-800 dark:text-white backdrop-blur-md shadow-lg flex items-center justify-between">
+        <img
+          src={PlaceholderLogo}
+          alt="Logo"
+          className="h-12 w-28"
+          title="Logo"
+        />
+        <nav className="hidden gap-8 md:flex">
+          {Object.entries(NavLinks).map(([title, link]) => (
+            <StyledNavLink to={link} key={title}>
+              {title}
+            </StyledNavLink>
+          ))}
+        </nav>
+        <span className="flex flex-row gap-4">
+          <MagnifyingGlassIcon className="h-6 text-slate-500" />
+          <button
+            title="Menu"
+            onClick={toggleSidebar}
+            className="opacity-50 hover:opacity-100 md:hidden"
+          >
+            <Bars3Icon className="h-6" />
+          </button>
+        </span>
+      </header>
       <SideBar open={modalOpen} onClose={handleSidebarClose} />
-      <MagnifyingGlassIcon className="h-6 text-slate-500" />
-      <button
-        title="Menu"
-        onClick={toggleSidebar}
-        className="opacity-50 hover:opacity-100 md:hidden"
-      >
-        <Bars3Icon className="h-6" />
-      </button>
-    </header>
+    </>
   );
 }
