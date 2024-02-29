94dbb245c08c6322e99b6097abe65c72fff4d08f
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 18:08:45 +0530

    refactor: use search filtered products to for the products page

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -3,15 +3,19 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { Pagination } from "../../components/Pagination";
 import { ProductCard } from "../../components/ProductCard";
 import { Products } from "./data";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 export function ProductsPage(): JSX.Element {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState("");
+  const filteredProducts = useMemo(
+    () => Products.filter(({ title }) => new RegExp(search, "i").test(title)), // TODO: remove it and use correct data
+    [search],
+  );
   const productsPerPage = window.matchMedia("(max-width: 768px)").matches
     ? 10
     : 20;
-  const pageLength = Math.ceil(Products.length / productsPerPage);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [search, setSearch] = useState("");
+  const pageLength = Math.ceil(filteredProducts.length / productsPerPage);
 
   const handleSearchChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setSearch(e.target.value);
@@ -23,7 +27,6 @@ export function ProductsPage(): JSX.Element {
         <span className="text-red-600">Flash Deals,</span> Enjoy Upto 70%
         discounts
       </h3>
-      {/* TODO: add search product logic */}
       <div className="flex flex-row border rounded-xl px-4 py-2 gap-2">
         <MagnifyingGlassIcon className="h-6 text-slate-500" />
         <input
@@ -36,12 +39,12 @@ export function ProductsPage(): JSX.Element {
         />
       </div>
       <div className="grid md:grid-cols-3 grid-cols-1 gap-x-8 gap-y-8">
-        {/* TODO: remove it and use correct data */}
-        {Products.slice(
-          productsPerPage * (currentPage - 1),
-          productsPerPage * (currentPage - 1) + productsPerPage,
-        )
-          .filter(({ title }) => new RegExp(search, "i").test(title))
+        {filteredProducts
+          .slice(
+            productsPerPage * (currentPage - 1),
+            productsPerPage * (currentPage - 1) + productsPerPage,
+          )
+
           .map((product) => (
             <ProductCard key={product.title} {...product} />
           ))}
@@ -50,10 +53,10 @@ export function ProductsPage(): JSX.Element {
         <p className="font-semibold text-sky-400 text-center">
           Showing {productsPerPage * (currentPage - 1) + 1} -{" "}
           {productsPerPage * (currentPage - 1) + productsPerPage >
-          Products.length
-            ? Products.length
+          filteredProducts.length
+            ? filteredProducts.length
             : productsPerPage * (currentPage - 1) + productsPerPage}{" "}
-          products out of total {Products.length} products
+          products out of total {filteredProducts.length} products
         </p>
         <Pagination
           pageLength={pageLength}
