f01f38233353ee19dbfec3ac451e6078627e06d4
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 23:51:34 +0530

    fix: issue of server loading script on / only

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,34 +1,29 @@
 import { App } from "./App";
 import { NavLinks } from "./components/utils";
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import { AboutUs, ContactUs, Home, Products } from "./code-splits";
 
-export const routes = createBrowserRouter(
-  [
-    {
-      path: NavLinks.Home,
-      element: <App />,
-      children: [
-        {
-          path: NavLinks.Home,
-          element: <Home />,
-        },
-        {
-          path: NavLinks["About Us"],
-          element: <AboutUs />,
-        },
-        {
-          path: NavLinks.Products,
-          element: <Products />,
-        },
-        {
-          path: NavLinks["Contact Us"],
-          element: <ContactUs />,
-        },
-      ],
-    },
-  ],
+export const routes = createHashRouter([
   {
-    basename: "/computer-distributor", // Since vite base has been changed to this for gh-pages. TODO: remove it if needed when deployed to some other server
+    path: NavLinks.Home,
+    element: <App />,
+    children: [
+      {
+        path: NavLinks.Home,
+        element: <Home />,
+      },
+      {
+        path: NavLinks["About Us"],
+        element: <AboutUs />,
+      },
+      {
+        path: NavLinks.Products,
+        element: <Products />,
+      },
+      {
+        path: NavLinks["Contact Us"],
+        element: <ContactUs />,
+      },
+    ],
   },
-);
+]);
