a212a0da9790799acd300db9d4b6e0b8d87dcb43
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 23:42:50 +0530

    refactor: make loading overlay theme oriented

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { LoadingOverlay } from "./components/LoadingOverlay";
+import { useTheme } from "./hooks/theme";
 import { Outlet, useLocation } from "react-router-dom";
 import { Suspense, useEffect } from "react";
 
 export function App(): JSX.Element {
   const location = useLocation();
+  const { theme } = useTheme();
 
   useEffect(() => {
     window.scrollTo({ top: 0, behavior: "smooth" });
@@ -15,7 +17,7 @@ export function App(): JSX.Element {
     <div className="font-barlow flex flex-col items-center gap-8 dark:bg-[#121212] dark:text-white">
       <Header />
       <main className="container min-h-screen mt-12 md:px-0 px-12">
-        <Suspense fallback={<LoadingOverlay />}>
+        <Suspense fallback={<LoadingOverlay theme={theme} />}>
           <Outlet />
         </Suspense>
       </main>
--- src/components/LoadingOverlay/index.tsx
+++ src/components/LoadingOverlay/index.tsx
@@ -1,9 +1,17 @@
+import type { Theme } from "../ThemeProvider/utils";
 import styles from "./index.module.css";
 
-export function LoadingOverlay(): JSX.Element {
+interface LoadingOverlayProps {
+  theme: Theme;
+}
+
+export function LoadingOverlay({ theme }: LoadingOverlayProps): JSX.Element {
   return (
     <div className="w-full h-full fixed top-0 left-0 flex items-center justify-center">
-      <span className={styles.loader}></span>
+      <span
+        className={styles.loader}
+        style={{ borderColor: theme === "dark" ? "#FFFFFF" : "#000000" }}
+      ></span>
     </div>
   );
 }
