4afb02e8a8ea8071308d976fe65ba6860ace64d8
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 20:09:56 +0530

    refactor: added scrolling on location change

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { LoadingOverlay } from "./components/LoadingOverlay";
-import { Outlet } from "react-router-dom";
-import { Suspense } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import { Suspense, useEffect } from "react";
 
 export function App(): JSX.Element {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [location.pathname]);
   return (
     <div className="font-barlow flex flex-col items-center gap-8 dark:bg-[#121212] dark:text-white">
       <Header />
