00556849170408366d33dbcc79cec49695018d9a
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 18:53:05 +0530

    refactor: do not show pagination when filtered products is zero

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -53,21 +53,23 @@ export function ProductsPage(): JSX.Element {
           </p>
         )}
       </div>
-      <div className="flex flex-col items-center justify-center gap-8">
-        <p className="font-semibold text-sky-400 text-center">
-          Showing {productsPerPage * (currentPage - 1) + 1} -{" "}
-          {productsPerPage * (currentPage - 1) + productsPerPage >
-          filteredProducts.length
-            ? filteredProducts.length
-            : productsPerPage * (currentPage - 1) + productsPerPage}{" "}
-          products out of total {filteredProducts.length} products
-        </p>
-        <Pagination
-          pageLength={pageLength}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        />
-      </div>
+      {filteredProducts.length > 0 ? (
+        <div className="flex flex-col items-center justify-center gap-8">
+          <p className="font-semibold text-sky-400 text-center">
+            Showing {productsPerPage * (currentPage - 1) + 1} -{" "}
+            {productsPerPage * (currentPage - 1) + productsPerPage >
+            filteredProducts.length
+              ? filteredProducts.length
+              : productsPerPage * (currentPage - 1) + productsPerPage}{" "}
+            products out of total {filteredProducts.length} products
+          </p>
+          <Pagination
+            pageLength={pageLength}
+            setCurrentPage={setCurrentPage}
+            currentPage={currentPage}
+          />
+        </div>
+      ) : undefined}
     </div>
   );
 }
