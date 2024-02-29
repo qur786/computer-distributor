74f387a122b76b8ba93263612d499667d835a7ed
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 18:47:25 +0530

    refactor: update product card as per dark mode

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -12,8 +12,12 @@ export function ProductCard({
   imageAlt,
 }: ProductCardProps): JSX.Element {
   return (
-    <div className="shadow-top-shadow h-96 rounded-md flex flex-col items-center justify-between">
-      <img src={image} alt={imageAlt} className="h-[80%] w-full object-cover" />
+    <div className="shadow-top-shadow dark:bg-[#1E1E1E] h-96 rounded-md flex flex-col items-center justify-between">
+      <img
+        src={image}
+        alt={imageAlt}
+        className="h-[80%] w-full object-cover rounded-t-md"
+      />
       <h6 className="text-xl font-semibold text-center">{title}</h6>
       <p className="mb-4 text-center">{price} &#8377;</p>
     </div>
