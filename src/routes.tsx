1f4c56612cdb77088b157b016ccf64de89b6b1fa
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-02-29 23:22:02 +0530

    fix: react-router-dom issue with changed base of vite

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,27 +3,32 @@ import { NavLinks } from "./components/utils";
 import { createBrowserRouter } from "react-router-dom";
 import { AboutUs, ContactUs, Home, Products } from "./code-splits";
 
-export const routes = createBrowserRouter([
+export const routes = createBrowserRouter(
+  [
+    {
+      path: NavLinks.Home,
+      element: <App />,
+      children: [
+        {
+          path: NavLinks.Home,
+          element: <Home />,
+        },
+        {
+          path: NavLinks["About Us"],
+          element: <AboutUs />,
+        },
+        {
+          path: NavLinks.Products,
+          element: <Products />,
+        },
+        {
+          path: NavLinks["Contact Us"],
+          element: <ContactUs />,
+        },
+      ],
+    },
+  ],
   {
-    path: NavLinks.Home,
-    element: <App />,
-    children: [
-      {
-        path: NavLinks.Home,
-        element: <Home />,
-      },
-      {
-        path: NavLinks["About Us"],
-        element: <AboutUs />,
-      },
-      {
-        path: NavLinks.Products,
-        element: <Products />,
-      },
-      {
-        path: NavLinks["Contact Us"],
-        element: <ContactUs />,
-      },
-    ],
+    basename: "/computer-distributor", // Since vite base has been changed to this for gh-pages. TODO: remove it if needed when deployed to some other server
   },
-]);
+);
