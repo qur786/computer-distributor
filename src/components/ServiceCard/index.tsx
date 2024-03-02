fc32be9d44db51b90cde9a62b1c0739012566456
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-03 00:11:22 +0530

    refactor: increase service card height

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -12,7 +12,7 @@ export function ServiceCard({
   title,
 }: ServiceCardProps): JSX.Element {
   return (
-    <div className="flex flex-row gap-2 rounded-md h-60 w-full shadow-md dark:bg-[#1E1E1E] box-border p-2">
+    <div className="flex flex-row gap-2 rounded-md h-72 w-full shadow-md dark:bg-[#1E1E1E] box-border p-2">
       <img src={image} alt={imageAlt} className="w-1/2 h-full object-contain" />
       <div className="flex flex-col gap-2 w-1/2 h-full">
         <h6 className="text-xl font-bold text-[#0F3460] dark:text-white">
