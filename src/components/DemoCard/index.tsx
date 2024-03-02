693755c66a19980ce75349f95759105a75427825
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-02 21:34:44 +0530

    refactor: updated color for the title in the demo card

--- src/components/DemoCard/index.tsx
+++ src/components/DemoCard/index.tsx
@@ -15,7 +15,9 @@ export function DemoCard({
     <div className="flex flex-row gap-2 rounded-md h-48 w-full shadow-md dark:bg-[#1E1E1E] box-border p-2">
       <img src={image} alt={imageAlt} className="w-1/2 h-full" />
       <div className="flex flex-col gap-2 w-1/2 h-full">
-        <h6 className="text-xl font-bold">{title}</h6>
+        <h6 className="text-xl font-bold text-[#0F3460] dark:text-white">
+          {title}
+        </h6>
         <p>{description}</p>
       </div>
     </div>
