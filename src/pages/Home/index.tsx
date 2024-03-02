8be7751be23596f2e078a3893c46c6c77864dcd2
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-02 20:06:19 +0530

    refactor: minor

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,7 +8,7 @@ export function HomePage(): JSX.Element {
         <div className="w-full md:w-1/2">
           <h6 className="text-6xl md:mt-12 md:ml-12 font-bold text-[#212b36] dark:text-white text-center">
             Shop your new Device with{" "}
-            <span className="text-[#fa541c]">Computer Distributer</span>
+            <span className="text-[#fa541c]">Computer Distributor</span>
           </h6>
         </div>
         <div className="w-full md:w-1/2 hidden md:block">
@@ -16,17 +16,17 @@ export function HomePage(): JSX.Element {
         </div>
       </div>
       <div className="h-screen md:w-full w-[90%] rounded-xl bg-slate-100 dark:bg-slate-900 py-4 px-2 box-border flex flex-col md:gap-8">
-        <div className="h-1/2 md:h-4/6 flex flex-col justify-center items-center">
+        <div className="h-1/2 flex flex-col justify-center items-center">
           <img
             src={BackgroundImage2}
             alt="store"
             className="h-full object-contain"
           />
         </div>
-        <div className="h-1/2 md:h-2/6 flex flex-col items-center justify-between">
+        <div className="h-1/2 flex flex-col items-center justify-center gap-4">
           <p className="text-sm font-bold text-center">NEW START</p>
-          <h5 className="text-[#fa541c] font-semibold text-6xl text-center">
-            Computer Distributer
+          <h5 className="text-[#fa541c] font-semibold text-5xl text-center">
+            Computer Distributor
           </h5>
           <p className="text-center">
             Modern ui kit to save your time, boost your creativity.
