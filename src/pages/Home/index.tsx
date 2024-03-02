07db16bb4ff447e8b43700f4e6f9c03a826da8a9
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-03 00:40:55 +0530

    refactor: stream line

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -54,49 +54,49 @@ export function HomePage(): JSX.Element {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <ServiceCard
             image={BestComputer}
-            imageAlt={"Best Computers"}
+            imageAlt="Best Computers"
             title="Best Computers"
             description="Discover the finest selection of computers and laptops at our shop. From cutting-edge performance to sleek designs, find the perfect device to suit your needs and elevate your computing experience."
           />
           <ServiceCard
             image={RepairImage}
-            imageAlt={"Fast Repair"}
+            imageAlt="Fast Repair"
             title="Fast Repair"
             description="Trust our skilled technicians to fix your computer issues promptly and efficiently. Get your device back up and running smoothly with our reliable repair services."
           />
           <ServiceCard
             image={AccessoriesImage}
-            imageAlt={"Accessories"}
+            imageAlt="Accessories"
             title="Accessories"
             description="Explore our wide range of accessories, including keyboards, mouse, monitors, motherboard, HDD, SSD, printers, hedaphones, and more, to enhance your computing experience."
           />
           <ServiceCard
             image={OnsiteRepair}
-            imageAlt={"On-Site Repairs"}
+            imageAlt="On-Site Repairs"
             title="On-Site Repairs"
             description="Can't make it to our shop? No problem. We offer on-site repair services to conveniently fix your computer issues at your location."
           />
           <ServiceCard
             image={CustomPC}
-            imageAlt={"Custom PC Builds"}
+            imageAlt="Custom PC Builds"
             title="Custom PC Builds"
             description="Let us tailor-make the perfect PC for your needs, whether it's for gaming, professional work, or everyday use."
           />
           <ServiceCard
             image={BugFix}
-            imageAlt={"Virus & Malware Removal"}
+            imageAlt="Virus & Malware Removal"
             title="Virus & Malware Removal"
             description="Say goodbye to pesky viruses and malware. Our experts will thoroughly clean your system and ensure it's protected against future threats."
           />
           <ServiceCard
             image={DataRecovery}
-            imageAlt={"Data Recovery"}
+            imageAlt="Data Recovery"
             title="Data Recovery"
             description="Accidentally deleted important files? Don't panic. We specialize in retrieving lost data from hard drives, SSDs, and other storage devices."
           />
           <ServiceCard
             image={HardwareUpgrade}
-            imageAlt={"Hardware Upgrades"}
+            imageAlt="Hardware Upgrades"
             title="Hardware Upgrades"
             description="Boost your computer's performance with our hardware upgrade services. From RAM and graphics cards to storage drives, we'll help you take your system to the next level."
           />
