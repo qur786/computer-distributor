8cd7115a115403ecce8c9d2a0f5a6755ac1d9de6
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-03 00:59:41 +0530

    refactor: increase vertical padding in serice card

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -14,7 +14,7 @@ export function ServiceCard({
   return (
     <div className="flex flex-row gap-2 rounded-md h-72 w-full shadow-md dark:bg-[#1E1E1E] box-border py-2 px-4">
       <img src={image} alt={imageAlt} className="w-1/2 h-full object-contain" />
-      <div className="flex flex-col gap-2 w-1/2 h-full">
+      <div className="flex flex-col gap-2 w-1/2 h-full py-2">
         <h6 className="text-xl font-bold text-[#0F3460] dark:text-white">
           {title}
         </h6>
