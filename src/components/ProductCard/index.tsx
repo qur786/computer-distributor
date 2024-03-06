db332bf753f904609438c7f48662d5d21bcb46d4
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-06 19:25:42 +0530

    refactor: update product card style

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,8 +1,9 @@
 export interface ProductCardProps {
   title: string;
-  price?: string;
+  price?: number;
   image: string;
   imageAlt: string;
+  description: string;
 }
 
 export function ProductCard({
@@ -10,6 +11,7 @@ export function ProductCard({
   price,
   image,
   imageAlt,
+  description,
 }: ProductCardProps): JSX.Element {
   return (
     <div className="shadow-top-shadow dark:bg-[#1E1E1E] h-96 rounded-md flex flex-col items-center justify-between">
@@ -17,14 +19,17 @@ export function ProductCard({
         src={image}
         alt={imageAlt}
         loading="lazy"
-        className="h-[80%] w-full object-cover rounded-t-md"
+        className="h-[70%] w-full object-cover rounded-t-md"
       />
-      <div className="my-4 h-16">
-        <h6 className="text-xl font-semibold text-center text-[#0A79DF]">
+      <div className="my-4 mx-2 h-[30%] flex flex-col gap-2">
+        <h6 className="text-xl font-semibold text-center text-[#2F363F]">
           {title}
         </h6>
+        <p className="text-center text-slate-700 text-sm line-clamp-1">
+          {description}
+        </p>
         {price ? (
-          <p className="text-center text-[#3498DB]">{price} &#8377;</p>
+          <p className="text-center text-[#4C4B4B]">{price} &#8377;</p>
         ) : undefined}
       </div>
     </div>
