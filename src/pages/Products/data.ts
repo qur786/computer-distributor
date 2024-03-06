0b636d859d57be909656065039385a19842eb611
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-06 19:25:55 +0530

    refactor: added dell product card

--- src/pages/Products/data.ts
+++ src/pages/Products/data.ts
@@ -1,25 +1,11 @@
+import DellLaptopImage from "./dell-laptop.jpg";
 import type { ProductCardProps } from "../../components/ProductCard";
 
 export const Products: ProductCardProps[] = [
   {
-    title: "Sony Xperia Digital Camera",
-    price: "37000",
-    image:
-      "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imageAlt: "Acer computer",
-  },
-  {
-    title: "Sony Xperia Digital Camera",
-    price: "37000",
-    image:
-      "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imageAlt: "Acer computer",
-  },
-  {
-    title: "Sony Xperia Digital Camera",
-    price: "37000",
-    image:
-      "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    imageAlt: "Acer computer",
+    title: "Dell laptops and computers",
+    image: DellLaptopImage,
+    imageAlt: "Dell computer",
+    description: "We have all kinds of dell computers and laptops.",
   },
 ];
