657f31766f9ac0f817078dc048e210ed0823f3fd
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-07 18:29:30 +0530

    refactor: fix card height issue

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -18,15 +18,15 @@ export function ProductCard({
   imageTitle,
 }: ProductCardProps): JSX.Element {
   return (
-    <div className="shadow-top-shadow dark:bg-[#1E1E1E] h-96 rounded-md flex flex-col items-center justify-between">
+    <div className="shadow-top-shadow dark:bg-[#1E1E1E] h-[396px] rounded-md">
       <img
         src={image}
         alt={imageAlt}
         loading="lazy"
         title={imageTitle}
-        className="h-[65%] w-full object-cover rounded-t-md"
+        className="h-64 w-full object-cover rounded-t-md"
       />
-      <div className="my-4 mx-2 h-[35%] flex flex-col gap-2">
+      <div className="my-4 mx-2 flex flex-col gap-2">
         <h6 className="text-xl font-semibold text-center text-[#2F363F] dark:text-slate-100">
           {title}
         </h6>
