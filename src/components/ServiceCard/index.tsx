d153b77cc5a9df92f1fbf798dfde80b6d3084438
Qurban Ahmad <ahmad76qurban@gmail.com>
2024-03-19 17:20:33 +0530

    refactor: update service card to col layout in mobile view

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -12,15 +12,15 @@ export function ServiceCard({
   title,
 }: ServiceCardProps): JSX.Element {
   return (
-    <div className="flex flex-row gap-2 rounded-md h-96 md:h-72 w-full shadow-md dark:bg-[#1E1E1E] box-border py-2 px-4">
+    <div className="flex flex-col md:flex-row gap-4 md:gap-2 rounded-md h-96 md:h-72 w-full shadow-md dark:bg-[#1E1E1E] box-border py-2 px-4">
       <img
         src={image}
         alt={imageAlt}
         loading="lazy"
-        className="w-1/2 h-full object-contain"
+        className="w-full h-1/2 md:w-1/2 md:h-full object-contain"
       />
-      <div className="flex flex-col gap-2 w-1/2 h-full py-2">
-        <h6 className="text-xl font-bold text-[#0F3460] dark:text-white">
+      <div className="flex flex-col gap-2 w-full h-1/2 md:w-1/2 md:h-full py-2">
+        <h6 className="text-xl font-bold text-[#0F3460] dark:text-white text-center md:text-start">
           {title}
         </h6>
         <p>{description}</p>
