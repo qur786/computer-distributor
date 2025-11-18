5b75446c2323dd4e70ad8f1f0921c1aa614db351
Qurban Ahmad <ahmad76qurban@gmail.com>
2025-11-18 10:53:00 +0530

    refactor update base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/computer-distributor/", // Ref: https://vitejs.dev/guide/static-deploy#github-pages
+  // base: "/computer-distributor/", // Ref: https://vitejs.dev/guide/static-deploy#github-pages
   plugins: [react()],
   server: {
     port: 3000,
